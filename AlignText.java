42930f973157fc93e972544f29c09b046a1c7387
bl41 <bl41@pc3-034-l.cs.st-andrews.ac.uk>
2017-09-25 15:55:49 +0100

    left align with no words being broke

--- AlignText.java
+++ AlignText.java
@@ -4,10 +4,12 @@ public class AlignText {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
+			
 			String[] parags = GetText.readFile(args[0]);
 			int column = Integer.valueOf(args[1]);
 			String[] words = SpiltText.splitText(parags);
 			PrintText.printText(words, column);
+			
 		}catch(ArrayIndexOutOfBoundsException e){
 			System.out.println("usage: java AlignText file_name line_length");
 		}
--- PrintText.java
+++ PrintText.java
@@ -2,6 +2,21 @@
 public class PrintText {
 
 	public static void printText(String[] words, int column){
+		int pLength = 0;
+		int remain = column;
 		
+		for(int i = 0;i < words.length; i++){
+			if(words[i].length() <= remain && remain != 0){
+				System.out.print(words[i]);
+				pLength = pLength + words[i].length();
+				remain = column - pLength;
+			}else if(words[i].length() > remain || remain == 0){
+				System.out.print("\n" + words[i]);
+				pLength = words[i].length();
+				remain = column - pLength;
+			}
+		}
+		
+		System.out.print("\n");
 	}
 }
