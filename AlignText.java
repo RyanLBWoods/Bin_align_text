2c560d198b8dc6b4f7abd1c2656476707fd4c7f2
bl41 <bl41@pc3-024-l.cs.st-andrews.ac.uk>
2017-10-05 17:54:01 +0100

    add user input written file name, update code style

--- AlignText.java
+++ AlignText.java
@@ -19,37 +19,42 @@ public class AlignText {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         try {
-
             // Get text from file
             String[] parags = GetText.readFile(args[0]);
             // Get expected column from user input
             int column = Integer.valueOf(args[1]);
             ArrayList<String> tofile = new ArrayList<String>();
             ArrayList<String> temp = new ArrayList<String>();
-            // Initiate file writer
+            final int basicArgsLength = 2;
+            final int wFileName = 3;
+            final int extraArgsLength = 4;
             String wfname = "";
             char mode;
             // Get expected align mode
-            if (args.length == 2) {
+            if (args.length == basicArgsLength) {
                 mode = 'R';
             } else {
                 mode = args[2].charAt(0);
             }
             // Get expected file to be written in
-            if(args.length == 4){
-                wfname = args[3];
+            if (args.length == extraArgsLength) {
+                wfname = args[wFileName];
             } else {
+                // Set "new.txt" as default new file if user didn't input any specific name
                 wfname = "new.txt";
             }
+            // Initiate file writer
             FileWriter fw = new FileWriter(wfname);
             BufferedWriter bw = new BufferedWriter(fw);
-            
+            // Process the text
             for (int i = 0; i < parags.length; i++) {
                 // Invoke split function to split words of text
                 String[] words = SpiltText.splitText(parags[i]);
-                // Invoke align and print function to align text and print
-                // according to the input column
-
+                /*
+                 * Invoke align and print function to align text and print
+                 * according to the input column (for extra function)Stored the
+                 * processed text into an array list of string
+                 */
                 String[] alignedTx = PrintText.printText(words, column, mode);
                 for (int j = 0; j < alignedTx.length; j++) {
                     tofile.add(alignedTx[j]);
@@ -62,13 +67,12 @@ public class AlignText {
                 }
             }
             tofile = temp;
-            //Write into file
+            // Write into file
             for (int n = 0; n < tofile.size(); n++) {
                 bw.write(tofile.get(n).toString());
                 bw.flush();
             }
             bw.close();
-
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("usage: java AlignText file_name line_length <align_mode>");
             System.exit(0);
